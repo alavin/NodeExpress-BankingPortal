ee617e9f413d5b70246ea263c5130a6511798b7b
alavin <vincenzomaria.alaia@gmail.com>
2019-06-27 12:08:01 +1000

    module 3 file handling and routes

--- src/app.js
+++ src/app.js
@@ -13,6 +13,9 @@ app.set('view engine', 'ejs');
 //point to the public dir where all css and js are
 app.use(express.static(path.join(__dirname, 'public'))); 
 
+//use URL Encoded Middleware
+app.use(express.urlencoded( { extended: true } )); 
+
 //read the content of a accountData file
 const accountData = fs.readFileSync(
     //two arguments - the absolute path to the file and the encoding
@@ -46,6 +49,38 @@ app.get('/checking', (req, res) => {
 app.get('/credit', (req, res) => {
     //we pass the account view and an object with the key value pair
     res.render( 'account', { account: accounts.credit } );
+});
+
+//rout to get transfer
+app.get('/transfer', (req, res) => {
+    res.render( 'transfer', {  } );
+});
+
+//rout to post transfer
+app.post('/transfer', (req, res) => {
+    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+
+    //display a confirmation message
+    res.render('transfer', { message: 'Transfer Completed'});
+});
+
+//rout to get payment
+app.get('/payment', (req, res) => {
+    res.render( 'payment', { account: accounts.credit });
+});
+
+//rout to psot payment
+app.post('/payment', (req, res) => {
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount, 10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+
+    //display a confirmation message
+    res.render('payment', { message: 'Payment Successful', account: accounts.credit });
 });
 
 //rout to profile
